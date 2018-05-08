5cf56998c3ae4a2b9af94ca265fe7b89afb61078
kjmosier <keithjmosier@gmail.com>
2018-05-08 15:41:14 -0700

    start wasd movement of Dot

--- js/app.js
+++ js/app.js
@@ -2,20 +2,49 @@ let dot = document.createElement('div');
 dot.className = "dot";
 let dotX = 50;
 let dotY = 50;
-dot.style.top = dotX + "px";
-dot.style.left = dotY + "px";
+
 
 arena.appendChild(dot);
 
 
-function moveDot() {
-  alert( 'Hello everyone!' );
-}
+
+
+
 
 function placeDot() {
-  alert( 'Hello everyone!' );
+  dot.style.top = dotX + "px";
+  dot.style.left = dotY + "px";
 }
 
+
 xcoord.oninput = function() {
-  arena.innerHTML = xcoord.value;
+  dot.style.top = xcoord.value + "px";
 };
+
+
+document.addEventListener('keydown', function(event) {
+  switch (event.code){
+     case 'KeyS':
+       dotX+=6;
+       break;
+     case 'KeyW':
+       dotX -=6;
+       break;
+     case 'KeyA':
+       dotY-=6;
+       break;
+     case 'KeyD':
+       dotY+=6;
+       break;
+     case ('KeyS' && 'KeyD'):
+       dotY+=6;
+       dotX+=6;
+       break;
+
+     default:
+       break;
+     }
+    placeDot();
+});
+
+placeDot();
