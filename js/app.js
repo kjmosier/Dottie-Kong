fc63410c1149862658026f3e8af8b0e7179975be
kjmosier <kjamesmosier@gmail.com>
2018-05-11 14:55:37 -0700

    changing to platform scroller

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ var myGamePiece;
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece = new component(15, 30, "red", 10, 120);
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 530;
+        this.canvas.height = 310;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[2]);
         this.interval = setInterval(updateGameArea, 20);
@@ -21,12 +21,16 @@ var myGameArea = {
             myGameArea.keys[e.keyCode] = (e.type == "keydown");
         })
     },
+    stop : function() {
+      clearInterval(this.interval);
+    },
     clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+    this.type = type;
     this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
@@ -34,14 +38,24 @@ function component(width, height, color, x, y) {
     this.speedY = 0;
     this.x = x;
     this.y = y;
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
     this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     this.newPos = function() {
+        this.gravitySpeed += this.gravity;
         this.x += this.speedX;
-        this.y += this.speedY;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+        }
     }
 }
 
@@ -51,75 +65,11 @@ function updateGameArea() {
     myGamePiece.speedY = 0;
     if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
     if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
-    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
-    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
+    if (myGameArea.keys && myGameArea.keys[32]) {
+      if (myGamePiece.hitBottom()){
+         myGamePiece.gravity = -0.025;
+      }
+    }
     myGamePiece.newPos();
     myGamePiece.update();
 }
-
-
-
-/*
-var dot;
-
-function startGame() {
-  dot = new component(30,30,"red", 10, 120);
-  gameArena.start();
-}
-
-var gameArena = {
-  arena : document.createElement("canvas"),
-  start : function() {
-    this.arena.width = 480;
-    this.arena.height = 270;
-    this.arena.setAttribute("id", "arena1")
-    this.context = this.arena.getContext("2d");
-    document.body.insertBefore(this.arena, document.body.childNodes[2]);
-    this.interval = setInterval(updateGameArena, 20);
-  },
-  clear : function() {
-    this.context.clearRect(0,0, this.arena.width, this.arena.height);
-  }
-}
-
-function component(width, height, color, x, y){
-  this.width=width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function(){
-    ctx = gameArena.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-  this.newPos = function(){
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-}
-
-function updateGameArena(){
-  gameArena.clear();
-  dot.newPos();
-  dot.update();
-}
-
-
-funtion moveup(){
-  dot.speedY -= 1;
-}
-
-funtion movedown(){
-  dot.speedY += 1;
-}
-
-funtion moveleft(){
-  dot.speedX -= 1;
-}
-
-funtion moveright(){
-  dot.speedX += 1;
-}
-*/
