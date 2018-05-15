bd27cfc0a212572fbcc8001f1a8a876325977a2f
kjmosier <kjamesmosier@gmail.com>
2018-05-14 18:54:56 -0700

    Add
    decks

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
 
 var myGamePiece;
+var deck = [];
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 80, 75);
+    myGamePiece = new component(30, 30, "red", 0, 0);
+    deck[0] = new platform(300, 15, "blue", 40, 180);
+    deck[1] = new platform(250, 15, "blue", 80, 80);
     myGameArea.start();
 }
 
@@ -36,6 +39,21 @@ var myGameArea = {
 }
 
 
+// --------------    platform --------------
+
+function platform(width, height, color, x, y){
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+
 // --------------     Game Piece --------------
 function component(width, height, color, x, y, type) {
     this.type = type;
@@ -58,6 +76,9 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
         this.hitBottom();
+        for (i = 0; i < deck.length; i += 1){
+          this.againstWall(deck[i]);
+        }
     }
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
@@ -66,142 +87,49 @@ function component(width, height, color, x, y, type) {
             this.gravitySpeed = -(this.gravitySpeed * this.bounce);
         }
     }
+    //  -----  stop at Walls ---------
+    this.againstWall = function(otherobj) {
+            var myleft = this.x;
+            var myright = this.x + (this.width);
+            var mytop = this.y;
+            var mybottom = this.y + (this.height);
+            var otherleft = otherobj.x;
+            var otherright = otherobj.x + (otherobj.width);
+            var othertop = otherobj.y;
+            var otherbottom = otherobj.y + (otherobj.height);
+            // Hit Top
+            if ((mytop < otherbottom) && (mybottom > otherbottom) &&(myleft < otherright) && (myright > otherleft)){
+              this.y = otherbottom;
+              this.gravitySpeed = 0;
+            }
+            //Hit Right
+            if ((mytop < otherbottom) && (mybottom > otherbottom) &&(myleft < otherright) && (myright > otherleft)){
+              this.y = otherbottom;
+              this.gravitySpeed = 0;
+            }
+            //Hit Bottom
+            if ((mybottom > othertop) && (mytop < othertop) &&(myleft < otherright) && (myright > otherleft)){
+              this.y = othertop - this.height;
+              this.gravitySpeed = 0;
+            }
+     }
 }
+
+// ----------------------  UPDATE FRAME ------------
 
 function updateGameArea() {
     myGameArea.clear();
-
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
     if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
     if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
     if (myGameArea.keys && myGameArea.keys[32]) {
-      if (myGamePiece.gravitySpeed == 0){myGamePiece.gravitySpeed = -5;}
-    }
+       if (myGamePiece.gravitySpeed == 0){myGamePiece.gravitySpeed = -5;}
+     }
+
     myGamePiece.newPos();
     myGamePiece.update();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-//break
-
-var dot;
-
-function startGame() {
-    myGameArea.start();
-    dot = new component(15, 30, "red", 10, 120);
-}
-
-
-//Arena
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 530;
-        this.canvas.height = 310;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[2]);
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = (e.type == "keydown");
-        })
-        window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = (e.type == "keydown");
-        })
-    },
-
-
-    stop : function() {
-      clearInterval(this.interval);
-    },
-
-
-    clear : function(){
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (i = 0; i < deck.length; i += 1){
+      deck[i].update();
     }
 }
-
-
-
-//Moving Game Piece
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.gamearea = myGameArea;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.gravity = 0.05;
-    this.gravitySpeed = 0;
-    this.jumpTime = 0;
-
-
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-
-    this.newPos = function() {
-        if (this.jumpTime == 0){
-          this.gravitySpeed += this.gravity;
-        } else {
-            this.jumpTime--;
-            if (this.jumpTime == 0){this.gravity = 0.05;}
-          }
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-
-
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-        }
-    }
-    this.jump = function(){
-      dot.gravity = -3.0;
-      dot.jumpTime = 2000;
-    }
-}
-
-function updateGameArea() {
-    myGameArea.clear();
-    dot.speedX = 0;
-    dot.speedY = 0;
-    if (myGameArea.keys && myGameArea.keys[37]) {dot.speedX = -1; }
-    if (myGameArea.keys && myGameArea.keys[39]) {dot.speedX = 1; }
-    if (myGameArea.keys && myGameArea.keys[32]) {
-      alert('spacebar');
-//       dot.jump();
-    }
-    dot.newPos();
-    dot.update();
-}
-
-*/
