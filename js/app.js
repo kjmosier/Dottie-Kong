06505fe3bbb16db17aef2f679009b4e2f0ca6677
kjmosier <keithjmosier@gmail.com>
2018-05-15 22:00:47 -0700

    Collision Detection

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ var myGamePiece;
 var deck = [];
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 0, 0);
-    deck[0] = new platform(300, 15, "blue", 40, 180);
-    deck[1] = new platform(250, 15, "blue", 80, 80);
+  myGamePiece = new component(30, 30, "red", 0, 0);
+  deck[0] = new platform(200, 15, "blue", 100, 225);
+  deck[1] = new platform(300, 15, "blue", 70, 240);
+  deck[2] = new platform(300, 15, "blue",120, 310);
+
     myGameArea.start();
 }
 
@@ -15,8 +17,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 540;
+        this.canvas.height = 350;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -61,6 +63,8 @@ function component(width, height, color, x, y, type) {
     this.height = height;
     this.x = x;
     this.y = y;
+    this.prevX = x;
+    this.prevY = y;
     this.speedX = 0;
     this.speedY = 0;
     this.gravity = 0.1;
@@ -70,15 +74,17 @@ function component(width, height, color, x, y, type) {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.prevX = this.x;
+        this.prevY = this.y;
     }
     this.newPos = function() {
         this.gravitySpeed += this.gravity;
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
         for (i = 0; i < deck.length; i += 1){
-          this.againstWall(deck[i]);
+          this.collision(deck[i]);
         }
+        this.hitBottom();
     }
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
@@ -88,53 +94,57 @@ function component(width, height, color, x, y, type) {
         }
     }
 
-    //  -----  stop at Walls ---------
+    this.collisionFrom = function(obj){
+      var player_bottom = this.y + this.height;
+      var obj_bottom = obj.y + obj.height;
+      var player_right = this.x + this.width;
+      var obj_right = obj.x + obj.width;
 
-    this.againstWall = function(otherobj) {
-            var myleft = this.x;
-            var myright = this.x + (this.width);
-            var mytop = this.y;
-            var mybottom = this.y + (this.height);
-            var otherleft = otherobj.x;
-            var otherright = otherobj.x + (otherobj.width);
-            var othertop = otherobj.y;
-            var otherbottom = otherobj.y + (otherobj.height);
+      var b_collision = obj_bottom - this.y;
+      var t_collision = player_bottom - obj.y;
+      var l_collision = player_right - obj.x;
+      var r_collision = obj_right - this.x;
+      if ((t_collision < b_collision) && (t_collision < l_collision) && (t_collision < r_collision) ){
+          //Top collision
+          //alert ('Top');
+          this.y = this.prevY;
+          this.gravitySpeed = 0;
+        }
+        if (b_collision < t_collision && b_collision < l_collision && b_collision < r_collision){
+          //bottom collision
+          //alert ('Bottom');
+          this.y = this.prevY;
+          this.gravitySpeed = .5 ;
+        }
+        if (l_collision < r_collision && l_collision < t_collision && l_collision < b_collision)
+        {
+          //Left collision
+        //  alert ('Left');
+          this.x = this.prevX;
+        }
+        if (r_collision < l_collision && r_collision < t_collision && r_collision < b_collision )
+        {
+          //Right collision
+        //  alert ('Right');
+          this.x = this.prevX;
+        }
 
-            var didHit = false;
+    }
 
-            // Hit Top ... If Under Other Object
-            if ((mytop < otherbottom) && (mybottom > otherbottom) && (myleft < otherright) && (myright > otherleft)){
-              this.y = otherbottom;
-              this.gravitySpeed = 1;
-              this.gravity = 0.1;
-              didHit = true;
-            }
-            //Hit Bottom ... If Above Other Object
-            if ((mybottom > othertop) && (mytop < othertop) &&(myleft < otherright) && (myright > otherleft)&& !didHit){
-              this.y = othertop - this.height;
-              this.gravitySpeed = 0;
-              didHit = true;
-            }
+    // -------   colision ----------------
+    this.collision = function(other){
+      if (this.x < other.x + other.width &&
+        this.x + this.width > other.x &&
+        this.y < other.y + other.height &&
+        this.height + this.y > other.y) {
+          this.collisionFrom(other);
+        }
+      }
 
-            //  Hit Right
-            if ((((mytop > othertop) && (mytop < otherbottom))
-                   || ((mybottom >= othertop) && (mybottom <= otherbottom))) && (myright > otherleft) && (myleft < otherleft) && !didHit ){
-                this.x = otherleft - this.width;
-                this.gravitySpeed = 1;
-                this.gravity = 0.1;
-                didHit = true;
-            }
-            //  Hit Left
-            if ((((mytop > othertop) && (mytop < otherbottom))
-                   || ((mybottom >= othertop) && (mybottom <= otherbottom))) && (myleft < otherright) && (myright > otherright) && !didHit){
-                this.x = otherright;
-                this.gravitySpeed = 1;
-                this.gravity = 0.1;
-                didHit = true;
-            }
 
-     }
-}
+    }
+
+
 
 // ----------------------  UPDATE FRAME ------------
 
@@ -145,7 +155,7 @@ function updateGameArea() {
     if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
     if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
     if (myGameArea.keys && myGameArea.keys[32]) {
-       if (myGamePiece.gravitySpeed == 0){myGamePiece.gravitySpeed = -5;}
+       if (myGamePiece.gravitySpeed == 0){myGamePiece.gravitySpeed = -4;}
      }
 
     myGamePiece.newPos();
