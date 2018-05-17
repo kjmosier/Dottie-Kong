2e5fc91ebb4f2fb6d9d2e3312a6012c8a036c13e
kjmosier <keithjmosier@gmail.com>
2018-05-17 14:03:18 -0700

    Add Sounds

--- js/app.js
+++ js/app.js
@@ -3,13 +3,19 @@
 var myGamePiece;
 var deck = [];
 var jumpSound;
+var hitSound;
+var barrel = [];
+var barrelCount = 0;
+var barrelNumber = 0;
 
 function startGame() {
-  jumpSound = new sound("sounds/Funny-cartoon-jump-sound-effect.mp3");
+  jumpSound = new sound("sounds/jump.mp3");
+  hitSound = new sound("sounds/hit.wav");
+  jumpOnTopSound = new sound("sounds/jumpOnTop.wav");
 
-  myGamePiece = new component(30, 30, "red", 5, 300);
+  myGamePiece = new component(30, 30, "red", 5, 300, "dot");
   kong = new component (30,50, "brown", 260, 35);
-  barrel = new component (25, 25, "brown", 270, 35, "barrel");
+  barrel[barrelNumber] = new component (25, 25, "brown", 270, 35, "barrel");
 
 
   deck[0] = new platform(200, 15, "green", 150, 85);
@@ -23,7 +29,7 @@ function startGame() {
   deck[7] = new platform(540, 5, "blue", 0, 0);
   deck[8] = new platform(540, 5, "blue", 0, 345);
 
-    myGameArea.start();
+  myGameArea.start();
 }
 
 
@@ -44,6 +50,7 @@ var myGameArea = {
           myGameArea.keys[e.keyCode] = (e.type == "keydown");
         })
       },
+
 
      stop : function() {
         clearInterval(this.interval);
@@ -101,15 +108,11 @@ function component(width, height, color, x, y, type) {
         for (i = 0; i < deck.length; i += 1){
           this.collision(deck[i]);
         }
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = -(this.gravitySpeed * this.bounce);
+        for (i = 0; i < barrel.length; i += 1){
+          this.collision(barrel[i]);
         }
     }
+
 
     this.collisionFrom = function(obj){
       var player_bottom = this.y + this.height;
@@ -127,12 +130,20 @@ function component(width, height, color, x, y, type) {
           this.y = this.prevY;
           this.gravitySpeed = 0;
           if (this.type == "barrel"){this.gravitySpeed = -1;}
+          if (this.type == "dot" && obj.type == "barrel"){
+            jumpOnTopSound.play();
+            this.gravitySpeed = -2;
+          }
         }
         if (b_collision < t_collision && b_collision < l_collision && b_collision < r_collision){
           //bottom collision
           this.y = this.prevY;
           this.gravitySpeed = .5;
-
+          if (this.type == "dot" && obj.type == "barrel"){
+            hitSound.play();
+            this.gravitySpeed = -10;
+            myGameArea.stop();
+          }
 
         }
         if (l_collision < r_collision && l_collision < t_collision && l_collision < b_collision)
@@ -140,17 +151,25 @@ function component(width, height, color, x, y, type) {
           //Left collision
           this.x = this.prevX;
           if (this.type == "barrel"){this.speedX *= -1}
-
+          if (this.type == "dot" && obj.type == "barrel"){
+            hitSound.play();
+            this.gravitySpeed = -10;
+            myGameArea.stop();
+          }
         }
         if (r_collision < l_collision && r_collision < t_collision && r_collision < b_collision )
         {
           //Right collision
           this.x = this.prevX;
           if (this.type == "barrel"){this.speedX *= -1}
+          if (this.type == "dot" && obj.type == "barrel"){
+            hitSound.play();
+            this.gravitySpeed = -10;
+            myGameArea.stop();
+          }
         }
 
     }
-
     // -------   colision ----------------
     this.collision = function(other){
       if (this.x < other.x + other.width &&
@@ -166,7 +185,7 @@ function component(width, height, color, x, y, type) {
 
 
 
-// ----------------------  UPDATE FRAME ------------
+// ----------------------  UPDATE GAME AREA ------------
 
 function updateGameArea() {
     myGameArea.clear();
@@ -180,16 +199,20 @@ function updateGameArea() {
          jumpSound.play();
        }
      }
-
-    myGamePiece.newPos();
-    barrel.newPos();
-    myGamePiece.update();
-    barrel.update();
     kong.update();
-
+    myGamePiece.newPos();
+    for (i = 0; i < barrel.length; i += 1){
+      barrel[i].newPos();
+    }
+    myGamePiece.update();
+    for (i = 0; i < barrel.length; i += 1){
+       barrel[i].update();
+     }
     for (i = 0; i < deck.length; i += 1){
       deck[i].update();
     }
+    barrel.push(new component (25, 25, "brown", 270, 35, "barrel"));
+
 }
 
 
